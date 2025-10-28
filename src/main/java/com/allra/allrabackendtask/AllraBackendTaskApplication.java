3dce38fd58fad4bf6ab980fb3a52481b5e9bfd24
MAREEKO\marym <mehrikodes@gmail.com>
2025-10-28 16:47:44 +0900

    Project setup

---
+++ src/main/java/com/allra/allrabackendtask/AllraBackendTaskApplication.java
@@ -0,0 +1,13 @@
+package com.allra.allrabackendtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllraBackendTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllraBackendTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allra/allrabackendtask/AllraBackendTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allra.allrabackendtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllraBackendTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
